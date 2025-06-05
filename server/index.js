0da350b2bcce11dc81b81c7775c39f398caaab6f
cytra <luka.constant@hotmail.com>
2025-06-05 15:31:50 +0200

    First

--- server/index.js
+++ server/index.js
@@ -9,12 +9,12 @@ app.use(cors());
 
 admin.initializeApp({
     credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     }),
     databaseURL: process.env.FIREBASE_DB_URL,
-});
+  });  
 
 const db = admin.firestore();
 
